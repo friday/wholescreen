6f1ecd0abbadcf774c5560e89d2597dc956919b8
Albin Larsson <mail@albinlarsson.com>
2018-07-01 03:47:11 +0200

    Rollup: Use multiple outputs instead of multple configs

--- rollup.config.js
+++ rollup.config.js
@@ -5,18 +5,16 @@ const {name} = pkg;
 const amd = {id: name};
 const input = '.js/wholescreen.js';
 const banner = `/*! ${name} v${pkg.version} | (c) ${new Date().getFullYear()} ${pkg.author} | Licensed under ${pkg.license} */`;
-
 const comments = (node, {type, value}) => type === "comment2" && value.startsWith('!'); // keep banner when minifying
-const config = (format, {minify = false, isDefault = false} = {}) => {
-	const output = {name, amd, format, banner, sourcemap: minify, file: `dist/wholescreen${isDefault ? '' :  `.${format}`}${minify ? '.min' : ''}.js`};
-	const plugins = minify ? [terser({output: {comments}})] : [];
-	return {input, output, plugins};
-};
+
+const output = ['amd', 'cjs', 'es', 'umd']
+	.map(format => ({amd, banner, format, name, file: `dist/wholescreen${format === 'umd' ? '' : `.${format}`}.js`}));
 
 export default [
-	config('amd'),
-	config('cjs'),
-	config('es'),
-	config('umd', {isDefault: true}),
-	config('umd', {isDefault: true, minify: true}),
+	{ input, output },
+	{
+		input,
+		output: {banner, name, file: 'dist/wholescreen.min.js', format: 'umd', sourcemap: true},
+		plugins: [terser({output: {comments}})],
+	},
 ];
