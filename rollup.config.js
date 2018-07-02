9969d9d1b4a14eb3260e1ad836edace6cb5daf18
Albin Larsson <mail@albinlarsson.com>
2018-07-02 07:40:06 +0200

    Change rollup build to expose output of detect.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,26 @@
 import {terser} from 'rollup-plugin-terser';
 
 const pkg = require('./package');
-const {name} = pkg;
-const amd = {id: name};
-const input = '.js/wholescreen.js';
-const banner = `/*! ${name} v${pkg.version} | (c) ${new Date().getFullYear()} ${pkg.author} | Licensed under ${pkg.license} */`;
 const comments = (node, {type, value}) => type === "comment2" && value.startsWith('!'); // keep banner when minifying
+const formats = ['amd', 'cjs', 'es', 'umd'];
 
-const output = ['amd', 'cjs', 'es', 'umd']
-	.map(format => ({amd, banner, format, name, file: `dist/wholescreen${format === 'umd' ? '' : `.${format}`}.js`}));
+const buildFile = (name, title = name) => {
+	const input = `.js/${name}.js`;
+	const banner = `/*! ${title} v${pkg.version} | (c) ${new Date().getFullYear()} ${pkg.author} | Licensed under ${pkg.license} */`;
+	return [
+		{
+			input,
+			output: formats.map(format => ({banner, name, format, file: `dist/${name}.${format}.js`})),
+		},
+		{
+			input,
+			output: {banner, name, file: `dist/${name}.min.js`, format: 'umd', sourcemap: true},
+			plugins: [terser({output: {comments}})],
+		}
+	];
+};
 
 export default [
-	{ input, output },
-	{
-		input,
-		output: {banner, name, file: 'dist/wholescreen.min.js', format: 'umd', sourcemap: true},
-		plugins: [terser({output: {comments}})],
-	},
+	...buildFile('wholescreen', pkg.name),
+	...buildFile('detect', `${pkg.name} - detect.js`)
 ];
