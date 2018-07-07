d367f2148c7092fcaecf9999e62b79f3e231ce6a
Albin Larsson <mail@albinlarsson.com>
2018-07-07 04:05:59 +0200

    chore: Switch to rollup-plugin-typescript2 (no temporary js output)

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,23 @@
 import {terser} from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 const pkg = require('./package');
 const comments = (node, {type, value}) => type === "comment2" && value.startsWith('!'); // keep banner when minifying
 const formats = ['amd', 'cjs', 'es', 'umd'];
 
 const buildFile = (name, title = name) => {
-	const input = `.js/${name}.js`;
+	const input = `src/${name}.ts`;
 	const banner = `/*! ${title} v${pkg.version} | (c) ${new Date().getFullYear()} ${pkg.author} | Licensed under ${pkg.license} */`;
 	return [
 		{
 			input,
 			output: formats.map(format => ({banner, name, format, file: `dist/${name}.${format}.js`})),
+			plugins: [typescript()],
 		},
 		{
 			input,
 			output: {banner, name, file: `dist/${name}.min.js`, format: 'umd', sourcemap: true},
-			plugins: [terser({output: {comments}})],
+			plugins: [typescript(), terser({output: {comments}})],
 		}
 	];
 };
