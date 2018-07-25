3d08ded31f731667152b757d80781fff571c61b8
Albin Larsson <mail@albinlarsson.com>
2018-07-25 11:23:05 +0200

    fix: don't break require if document is missing (for node.js ssr)

--- src/detect.ts
+++ src/detect.ts
@@ -5,20 +5,24 @@ function combine(parts: string[][]): string[] {
 	return parts.reduce((a, b) => [].concat.apply([], a.map(str1 => b.map(str2 => `${str1}${str2}`))), ['']);
 }
 
-function getProp(container: Node, props: string[][]): string {
+function getProp(props: string[][], path?: string): string {
+	if (typeof document !== 'object') {
+		return '';
+	}
+	const container = path ? document[path] : document;
 	return combine(props).find(prop => Boolean(container && prop && prop in container)) || '';
 }
 
 const props = {
-	element: getProp(document, [prefix, fullscreen, ['Element']]),
-	exit: getProp(document, [prefix, ['Exit', 'exit', 'Cancel', 'cancel'], fullscreen]),
-	request: getProp(document.body, [prefix, ['request', 'Request'], fullscreen]),
-	supported: getProp(document, [prefix, fullscreen, ['Enabled']]),
+	element: getProp([prefix, fullscreen, ['Element']]),
+	exit: getProp([prefix, ['Exit', 'exit', 'Cancel', 'cancel'], fullscreen]),
+	request: getProp([prefix, ['request', 'Request'], fullscreen], 'body'),
+	supported: getProp([prefix, fullscreen, ['Enabled']]),
 };
 
 const events = {
-	change: getProp(document, [['on'], prefix, fullscreen, ['change', 'Change']]).slice(2),
-	error: getProp(document, [['on'], prefix, fullscreen, ['error', 'Error']]).slice(2),
+	change: getProp([['on'], prefix, fullscreen, ['change', 'Change']]).slice(2),
+	error: getProp([['on'], prefix, fullscreen, ['error', 'Error']]).slice(2),
 };
 
 export {events, props};
